ac791010e23024e4ae4c460c225d5b5f548337b2
LucasMailly <lucas.mailly1@gmail.com>
2022-10-31 14:44:17 +0100

    Article image + user avatar added to fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -43,6 +43,7 @@ class AppFixtures extends Fixture
                 'password' => '123456',
                 'roles' => ['ROLE_ADMIN'],
                 'isBlocked' => false,
+                'avatar' => 'https://static.thenounproject.com/png/139500-200.png',
             ],
             [
                 'username' => 'user1',
@@ -50,6 +51,7 @@ class AppFixtures extends Fixture
                 'password' => '123456',
                 'roles' => ['ROLE_INDIVIDUAL'],
                 'isBlocked' => false,
+                'avatar' => 'https://randomuser.me/api/portraits/men/'.rand(0, 99).'.jpg',
             ],
             [
                 'username' => 'user2',
@@ -65,6 +67,7 @@ class AppFixtures extends Fixture
                 'password' => '123456',
                 'roles' => ['ROLE_PRO'],
                 'isBlocked' => false,
+                'avatar' => 'https://randomuser.me/api/portraits/women/'.rand(0, 99).'.jpg',
             ],
             [
                 'noSiret' => '326895',
@@ -73,29 +76,36 @@ class AppFixtures extends Fixture
                 'password' => '123456',
                 'roles' => ['ROLE_PRO'],
                 'isBlocked' => true,
+                'avatar' => 'https://randomuser.me/api/portraits/men/'.rand(0, 99).'.jpg',
             ],
         ];
-
+        //First delete old avatars
+        $avatars_directory = 'public/uploads/images/avatars/';
+        $files = scandir($avatars_directory);
+        foreach ($files as $file) {
+            if ($file != '.' && $file != '..' && $file != 'default.jpg' && $file != 'default.png') {
+                unlink($avatars_directory . $file);
+            }
+        }
+        //Then add users
         foreach ($users as $user) {
             $user_object = new User();
-            if ($user['roles'] == ['ROLE_ADMIN']) {
-                $user_object->setEmail($user['email']);
-                $user_object->setPassword($this->passwordHasher->hashPassword($user_object, $user['password']));
-                $user_object->setRoles($user['roles']);
-                $user_object->setIsBlocked($user['isBlocked']);
-            } elseif ($user['roles'] == ['ROLE_INDIVIDUAL']) {
+            $user_object->setEmail($user['email']);
+            $user_object->setPassword($this->passwordHasher->hashPassword($user_object, $user['password']));
+            $user_object->setIsBlocked($user['isBlocked']);
+            $user_object->setRoles($user['roles']);
+            if (isset($user['avatar'])) {
+                $avatar = $this->curl_get_contents($user['avatar']);
+                $avatar_name = md5($user['avatar']) . '.jpg';
+                file_put_contents($avatars_directory . $avatar_name, $avatar);
+                $user_object->setAvatar($avatar_name);
+            }
+
+            if ($user['roles'] == ['ROLE_INDIVIDUAL']) {
                 $user_object->setUsername($user['username']);
-                $user_object->setEmail($user['email']);
-                $user_object->setPassword($this->passwordHasher->hashPassword($user_object, $user['password']));
-                $user_object->setRoles($user['roles']);
-                $user_object->setIsBlocked($user['isBlocked']);
             } elseif ($user['roles'] == ['ROLE_PRO']) {
                 $user_object->setNoSiret($user['noSiret']);
                 $user_object->setName($user['name']);
-                $user_object->setEmail($user['email']);
-                $user_object->setPassword($this->passwordHasher->hashPassword($user_object, $user['password']));
-                $user_object->setRoles($user['roles']);
-                $user_object->setIsBlocked($user['isBlocked']);
             }
             $manager->persist($user_object);
         }
@@ -104,21 +114,34 @@ class AppFixtures extends Fixture
 
         //Add articles
         $images = [
-            'Pantalon cargo','https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F59e39263-711c-443d-aec8-e358620e3dd7%2Fp1.jpeg?table=block&id=c8fb9aab-85e9-4db1-9c02-8f5ce50bcf57&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2',   
-            'Robe', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F58990fa0-e835-4976-b6fb-91684236cc5f%2FUntitled.png?table=block&id=9557aec7-d717-42c6-ba01-6f7fe31fa05c&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2',
-            'Pantalon', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6ca6bcde-0c6c-43ab-8011-3097a7e0ec43%2Fp2.jpeg?table=block&id=4bc44524-bcb4-4150-8346-73d5210efdac&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=370&userId=&cache=v2',
-            'Veste jaune', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0bb438a9-b2e9-4c2c-806d-16d6547a43e6%2FUntitled.png?table=block&id=dea30251-1d88-4937-8211-dc5652c53b11&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=1400&userId=&cache=v2',
-            'T-shirt jaune', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F39a7cd51-8a0f-4d07-9f70-462f0cd45e04%2Ft1.jpeg?table=block&id=e70ba314-2d4b-4791-b1be-fa71573db269&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=400&userId=&cache=v2',
-            'Jean', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd1d871b3-7ea3-4ad7-9203-e3c2c966e1f4%2FUntitled.png?table=block&id=501d908a-c92e-4486-a343-c5f97b5e8ab3&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=720&userId=&cache=v2',
-            'T-shirt noir', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F02eb68d3-38c3-4bba-8068-cdaf5b469f77%2Ft2.jpeg?table=block&id=26a44099-3a06-48bc-bafe-d9856a3f5acb&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2',
-            'T-shirt rouge', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6d6ab22a-3bba-4463-beae-6b511847399f%2Ft3.jpeg?table=block&id=877a3ad1-3ba9-4a6b-90eb-c775501d6cec&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2',
+            'Pantalon cargo' => ['jpeg', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F59e39263-711c-443d-aec8-e358620e3dd7%2Fp1.jpeg?table=block&id=c8fb9aab-85e9-4db1-9c02-8f5ce50bcf57&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2'],   
+            'Robe' => ['png', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F58990fa0-e835-4976-b6fb-91684236cc5f%2FUntitled.png?table=block&id=9557aec7-d717-42c6-ba01-6f7fe31fa05c&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2'],
+            'Pantalon' => ['jpeg', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6ca6bcde-0c6c-43ab-8011-3097a7e0ec43%2Fp2.jpeg?table=block&id=4bc44524-bcb4-4150-8346-73d5210efdac&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=370&userId=&cache=v2'],
+            'Veste jaune' => ['png', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0bb438a9-b2e9-4c2c-806d-16d6547a43e6%2FUntitled.png?table=block&id=dea30251-1d88-4937-8211-dc5652c53b11&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=1400&userId=&cache=v2'],
+            'T-shirt jaune' => ['jpeg', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F39a7cd51-8a0f-4d07-9f70-462f0cd45e04%2Ft1.jpeg?table=block&id=e70ba314-2d4b-4791-b1be-fa71573db269&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=400&userId=&cache=v2'],
+            'Jean' => ['png', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd1d871b3-7ea3-4ad7-9203-e3c2c966e1f4%2FUntitled.png?table=block&id=501d908a-c92e-4486-a343-c5f97b5e8ab3&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=720&userId=&cache=v2'],
+            'T-shirt noir' => ['jpeg', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F02eb68d3-38c3-4bba-8068-cdaf5b469f77%2Ft2.jpeg?table=block&id=26a44099-3a06-48bc-bafe-d9856a3f5acb&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2'],
+            'T-shirt rouge' => ['jpeg', 'https://rowan-comfort-e0c.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6d6ab22a-3bba-4463-beae-6b511847399f%2Ft3.jpeg?table=block&id=877a3ad1-3ba9-4a6b-90eb-c775501d6cec&spaceId=ab3e3a4d-9b9b-467c-819b-cfd55fc07fe0&width=450&userId=&cache=v2'],
         ];
-        foreach ($images as $libelle => $image) {
+        //First delete old article images except default images
+        $images_directory = 'public/uploads/images/articles/';
+        $files = scandir($images_directory);
+        foreach ($files as $file) {
+            if ($file != '.' && $file != '..' && $file != 'default.jpg' && $file != 'default.png') {
+                unlink($images_directory . $file);
+            }
+        }
+        //Then add articles and associated images and save them in database
+        foreach ($images as $libelle => $image_url) {
             $article = new Article();
-            $libelle_slug = $this->slugger->slug($libelle);
-            file_put_contents('public/uploads/articles'.$libelle_slug.'.jpg', $this->curl_get_contents($image));
+            // download image
+            $image = file_get_contents($image_url[1]);
+            $image_name = md5(uniqid()) . '.' . $image_url[0];
+            $image_path = 'public/uploads/images/articles/' . $image_name;
+            file_put_contents($image_path, $image);
+            $article->setImgArticle($image_name);
+            
             $article->setLibelle($libelle);
-            $article->setImgArticle($libelle_slug.'.jpg');
             // random price
             $article->setPrice(mt_rand(10, 100));
             // random date
