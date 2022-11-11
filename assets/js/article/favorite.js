2d87033ca20af72de369670b9630af754e145449
LucasMailly <lucas.mailly1@gmail.com>
2022-11-11 21:04:49 +0100

    fix favorite icon change on click

--- assets/js/article/favorite.js
+++ assets/js/article/favorite.js
@@ -6,10 +6,12 @@ $(document).on('click', '.like-btn', function (e) {
     const $this = $(this);
     const url = Routing.generate('app_article_' + $this.data('action'), {id: $this.data('id')});
 
-    // update button class, data-action and child icon class
+    // update button class, data-action and child icon
     $this.data('action', $this.data('action') === 'add' ? 'drop' : 'add');
     $this.toggleClass('btn-danger btn-outline-danger');
     $this.find('i').toggleClass('fa-solid fa-regular');
+    $this.find('svg').attr('data-prefix', $this.find('svg').data('prefix') === 'fas' ? 'far' : 'fas');
+
 
     // send ajax request, if it fails, revert button class
     $.ajax({
